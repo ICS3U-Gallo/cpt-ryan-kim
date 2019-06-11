ed2e628ea555811bf29318a03ab82d0243e94163
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-06-11 05:56:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,38 @@ import threading
 import time
 
 camera = camera()
-
 fish_screen = fish_screen()
 
-step = 0
-step_cap = 100
+end_time = 15 * 60 * 60 # Ends the program at 15:00 (3:00 pm)
+
+snapped = False
+
+def thread_process(alarm_start):
+
+    person = camera.snap() # 0 for left /// 1 for right
+
+    fish_screen.update_AVOID_POINTS(alarm_start, person)
 
 while True:
 
-    t1 = threading.Thread(target=fish_screen.update_avoid_point(camera.snap())) 
-    t1.start()
-    t1.join()
+    time.sleep(0.03)
+    seconds = int(time.time() % 10)
+
+    if fish_screen.alarm is not None:
+            
+        t1 = threading.Thread(target=thread_process, args=(int(time.time()),))
+
+        if t1.isAlive() is False: t1.join()
+
+        if seconds % fish_screen.snap_interval is not 0: snapped = False
+            
+        elif seconds % fish_screen.snap_interval is 0 and snapped is False:
+            
+            snapped = True
+            t1.start()
 
     fish_screen.update()
 
-    if step is step_cap: 
-        camera.end_session
+    if int(time.time()) is end_time: 
+        camera.end_session()
         break
-    step += 1
