d4af34850ca170a31bc2acec4d283ddcb96fbd2d
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-06-15 15:07:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from sub_modules import alarm
+from fish import *
+
 import cv2
 import numpy as np
-from cam import *
-from fish import *
+
 import threading
 import time
 
@@ -10,21 +12,12 @@ cap = cv2.VideoCapture(0)
 cap.set(3,420)
 cap.set(4,240)
 
-fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
 
 fish_screen = fish_screen()
 
 end_time = 15 * 60 * 60 # Ends the program at 15:00 (3:00 pm)
 
-class alarm:
-	def __init__(self,start, time):
-		self.start = start
-		self.end = start + time
-
-	def check(self):
-		if int(time.time()) >= self.end: return True
-		
-		else: return False
 
 class compile:
     def __init__(self):
@@ -33,12 +26,11 @@ class compile:
     def session(self):
 
         while True:
-
-            time.sleep(0.03)
             
             ret, frame = cap.read()
             fgmask = fgbg.apply(frame)
-
+            
+            cv2.imshow("cam",fgmask)
             if self.alarm is None:
                 t1 = threading.Thread(target=fish_screen.update_AVOID_POINTS, args=(fgmask,))
                 t1.start()
@@ -46,10 +38,11 @@ class compile:
                 self.alarm = alarm(int(time.time()), 2)
 
             elif self.alarm is not None:
-                if t1.isAlive() is False: t1.join()
+                if self.alarm.check() and t1.isAlive() is False: 
+                    self.alarm = None   
+                    t1.join()
 
-                if self.alarm.check(): self.alarm = None   
-
+            cv2.waitKey(1)
             fish_screen.update()
 
             if int(time.time()) is end_time: 
