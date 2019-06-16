1e7342c3e2099044637c66bac49e8d2a7d6a3f3a
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-06-16 16:18:01 -0400

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,4 +1,3 @@
-from sub_modules import split
 import numpy as np
 
 class pedestrian_detector:
@@ -6,15 +5,20 @@ class pedestrian_detector:
 
 		self.ar = img
 
+		#The miniumum pixel activity sum required for a "person" to be in frame on one side of the camera
 		self.density_thresh = 138465.0
 
+		#Variables for the sum of the left and right pixel movment
 		self.left_sum = 0
 		self.right_sum = 0
 
+		#Numpy sum method sums all the columns, and returns a new array or one-dimension
 		self.sum_thread = np.sum(self.ar, axis=0)
 
+		#The half way point of the image, used to differenciate between left side and right side of the image
 		half_count = len(self.sum_thread) // 2
 
+		#Add to the left or right side based on the half_count
 		for row_sum in range(len(self.sum_thread)):
 
 			if row_sum <= half_count:
@@ -22,5 +26,3 @@ class pedestrian_detector:
 			
 			else:
 				self.right_sum += self.sum_thread[row_sum]
-
-		print(self.left_sum, self.right_sum)
